6089b4e5934ca2a35c2b19fec9ac89b75ab06670
Dmitry <demosne@gmail.com>
2025-03-25 15:44:58 +0300

    Enhance MainPage with input fields for width and length, and add functionality to calculate the number of sheets based on user input. Update styles for input elements and sections.

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,25 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import data from "../../data/data.json";
 import config from "../../data/config.json";
 import css from "./MainPage.module.css";
 
 const configWidthItem = config.find((i) => i.key === "width");
-const configLenghtItem = config.find((i) => i.key === "legth");
+const configLengthItem = config.find((i) => i.key === "length");
+const SHEET_LENGTH = 1;
 
 const MainPage = function () {
   const [list, setList] = useState("");
   const [pipe, setPipe] = useState("");
+  //Width of construction
   const [width, setWidth] = useState(configWidthItem?.min);
-  const [lenght, setLenght] = useState(configLenghtItem?.min);
+  //Length of construction
+  const [length, setLenght] = useState(configLengthItem?.min);
+  const [strength, setStrength] = useState("");
+  const [numOfSheets, setNumOfSheets] = useState<number | null>(null);
+  const square = width! * length!;
 
   const handleWidtInput = (e: { target: { value: string } }) => {
     const inpValue = +e.target.value;
-
     if (inpValue < configWidthItem?.min! || inpValue > configWidthItem?.max!)
       return;
-
     setWidth(inpValue);
   };
+
+  const handleLenghtInput = (e: { target: { value: string } }) => {
+    const inpValue = +e.target.value;
+    if (inpValue < configLengthItem?.min! || inpValue > configLengthItem?.max!)
+      return;
+    setLenght(inpValue);
+  };
+
+  useEffect(() => {
+    if (list !== "") {
+      const listSquare =
+        data.find((i) => i.name === list)?.width! * SHEET_LENGTH;
+      const numOfSheets = Math.ceil(square / listSquare);
+      setNumOfSheets(numOfSheets);
+    } else {
+      setNumOfSheets(null);
+    }
+  }, [list, width, length]);
 
   return (
     <div className={css.mainFrame}>
@@ -49,6 +71,7 @@ const MainPage = function () {
         </div>
 
         <div>
+          <p>Width</p>
           <input
             type="number"
             placeholder="Width"
@@ -57,8 +80,33 @@ const MainPage = function () {
           />
           {width}
         </div>
+
+        <div>
+          <p>Length</p>
+          <input
+            type="number"
+            placeholder="Length"
+            value={length}
+            onChange={handleLenghtInput}
+          />
+          {length}
+        </div>
+
+        <div>
+          <select onChange={(e) => setStrength(e.target.value)}>
+            <option value="">Choose strength</option>
+            {config
+              .filter((i) => i.type === "frame")
+              .map((frame) => (
+                <option value={frame.name}>{frame.name}</option>
+              ))}
+          </select>
+          {strength}
+        </div>
       </div>
-      <div className={css.rightSection}></div>
+      <div className={css.rightSection}>
+        <div>{numOfSheets && <p>Количество листов: {numOfSheets}</p>}</div>
+      </div>
     </div>
   );
 };
