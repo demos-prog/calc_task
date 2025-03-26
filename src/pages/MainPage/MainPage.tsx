2aa8642ba63c04aa3ae4ff9d1c63ba28ae93d5de
Dmitry <demosne@gmail.com>
2025-03-26 08:38:39 +0300

    Add fix amount calculation and update MainPage layout with localized labels for dropdowns and inputs

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -18,6 +18,7 @@ const MainPage = function () {
   const [strength, setStrength] = useState("");
   const [numOfSheets, setNumOfSheets] = useState<number | null>(null);
   const [pipesTotalLength, setPipesTotalLength] = useState<number | null>(null);
+  const [fixAmount, setFixAmount] = useState<number | null>(null);
   const square = width! * length!;
 
   const handleWidtInput = (e: { target: { value: string } }) => {
@@ -39,6 +40,14 @@ const MainPage = function () {
       const listSquare =
         data.find((i) => i.name === list)?.width! * SHEET_LENGTH;
       const numOfSheets = Math.ceil(square / listSquare);
+      const material = data.find((i) => i.name === list)?.material!;
+
+      const item = config
+        .filter((i) => i.type === "fix")
+        .find((i) => i.key === material);
+
+      const value = item?.value!;
+      setFixAmount(square * value);
       setNumOfSheets(numOfSheets);
     } else {
       setNumOfSheets(null);
@@ -49,9 +58,7 @@ const MainPage = function () {
       const step = config.find((i) => i.name === strength)?.step! - pipeWidth;
 
       const pipesInWidth = Math.ceil(width! / step!) + 1;
-
       const pipesInLength = Math.ceil(length! / step!) + 1;
-
       const pipesTotalLength = pipesInWidth * length! + pipesInLength * width!;
 
       setPipesTotalLength(pipesTotalLength);
@@ -63,77 +70,64 @@ const MainPage = function () {
   return (
     <div className={css.mainFrame}>
       <div className={css.leftSection}>
-        <div>
-          <select value={list} onChange={(e) => setList(e.target.value)}>
-            <option value="">Choose sheet</option>
-            {data
-              .filter((i) => i.type === "list")
-              .map((list) => (
-                <option key={uuidv4()} value={list.name}>
-                  {list.name}
-                </option>
-              ))}
-          </select>
-          {list}
-        </div>
+        <select value={list} onChange={(e) => setList(e.target.value)}>
+          <option value="">Выберите лист</option>
+          {data
+            .filter((i) => i.type === "list")
+            .map((list) => (
+              <option key={uuidv4()} value={list.name}>
+                {list.name}
+              </option>
+            ))}
+        </select>
+
+        <select value={pipe} onChange={(e) => setPipe(e.target.value)}>
+          <option value="">Выберите трубу</option>
+          {data
+            .filter((i) => i.type === "pipe")
+            .map((pipe) => (
+              <option key={uuidv4()} value={pipe.name}>
+                {pipe.name}
+              </option>
+            ))}
+        </select>
 
         <div>
-          <select value={pipe} onChange={(e) => setPipe(e.target.value)}>
-            <option value="">Choose pipe</option>
-            {data
-              .filter((i) => i.type === "pipe")
-              .map((pipe) => (
-                <option key={uuidv4()} value={pipe.name}>
-                  {pipe.name}
-                </option>
-              ))}
-          </select>
-          {pipe}
-        </div>
-
-        <div>
-          <p>Width</p>
+          <p>Ширина</p>
           <input
             type="number"
             placeholder="Width"
             value={width}
             onChange={handleWidtInput}
           />
-          {width}
         </div>
 
         <div>
-          <p>Length</p>
+          <p>Длина</p>
           <input
             type="number"
             placeholder="Length"
             value={length}
             onChange={handleLenghtInput}
           />
-          {length}
         </div>
 
-        <div>
-          <select
-            value={strength}
-            onChange={(e) => setStrength(e.target.value)}
-          >
-            <option value="">Choose strength</option>
-            {config
-              .filter((i) => i.type === "frame")
-              .map((frame) => (
-                <option key={uuidv4()} value={frame.name}>
-                  {frame.name}
-                </option>
-              ))}
-          </select>
-          {strength}
-        </div>
+        <select value={strength} onChange={(e) => setStrength(e.target.value)}>
+          <option value="">Выберите прочность</option>
+          {config
+            .filter((i) => i.type === "frame")
+            .map((frame) => (
+              <option key={uuidv4()} value={frame.name}>
+                {frame.name}
+              </option>
+            ))}
+        </select>
       </div>
       <div className={css.rightSection}>
         <div>{numOfSheets && <p>Количество листов: {numOfSheets}</p>}</div>
+        <div>{numOfSheets && <p>Количество метизов: {fixAmount}</p>}</div>
         <div>
-          {pipesTotalLength && <p>Длинна труб (м): {pipesTotalLength}</p>}
+          {pipesTotalLength && <p>Длина труб (м): {pipesTotalLength}</p>}
         </div>
       </div>
     </div>
